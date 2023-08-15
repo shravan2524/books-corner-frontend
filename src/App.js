4813eb1b64ffece30029225478db906a5b376966
shravan2524 <shravanchenna6@gmail.com>
2023-08-15 14:13:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import React, {useState, useEffect } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Navigate
+} from "react-router-dom";
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+import Signup from './pages/signup/Signup';
+import Login from './pages/login/Login';
+import Search from './pages/search/Search';
+import BookInfo from './pages/search/BookInfo';
+import Post from './pages/post/Post';
+import Collections from './pages/collections/Collections';
+import { useNavigate } from "react-router-dom";
+import MyPost from './pages/mypost/MyPost';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Protected >
+    <Home />
+    </Protected>
+  },
+  {
+    path: "/profile",
+    element:<Protected ><Profile /></Protected>,
+  },
+  {
+    path: "/signup",
+    element:<Signup />,
+  },
+  {
+    path: "/login",
+    element:<Login />,
+  },
+  {
+    path: "/search",
+    element:<Protected><Search /></Protected>
+  },
+  {
+    path: "/post",
+    element:<Protected><Post /></Protected>
+  },
+  {
+    path: "/bookinfo/:id",
+      element:<Protected><BookInfo /></Protected>
+  },
+  {
+    path: "/profile/:username",
+      element:<Protected><Profile /></Protected>,
+  },
+  {
+    path: "/collections",
+      element:<Protected><Collections /></Protected>,
+  },
+  {
+    path: "/myposts",
+      element:<Protected><MyPost /></Protected>,
+  },
+]);
+function Protected({  children }) {
+  const navigate = useNavigate();
+  const isLoggedIn = localStorage.getItem("username");
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
 function App() {
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navbar />
+    <RouterProvider router={router} />
     </div>
   );
 }
